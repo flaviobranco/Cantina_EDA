3f998abae6a7109581025e4a61f3d56293c55835
Carlos Duarte Santos Gomes <cduartesg@hotmail.com>
2019-04-22 11:23:50 +0100

    Debugging final do Issue #9

--- Cantina_EDA/Mesa.h
+++ Cantina_EDA/Mesa.h
@@ -2,6 +2,9 @@
 #include "aluno_staff.h"
 struct mesa {
 	int capacidade;
+	int numero;
+	int ocupacao;
+	// lista das pessoas
 };
 void inserir_mesas(mesa * mesa, int posicao,int tamanho);
 int random_mesa();
--- Cantina_EDA/main.cpp
+++ Cantina_EDA/main.cpp
@@ -7,10 +7,10 @@ int main() {
 	int tam = rand() % 30 + 20;
 	int n_mesa = 0;//número de mesas
 	int rnd=random_mesa();
-	mesa mesa[20];//criar a variável struct mesa com o nome "mesa", determinei um limite máximo para o debugging do issue #9 no github
+	mesa *mesa = new struct mesa[rnd];//criar a variável struct mesa com o nome "mesa", determinei um limite máximo para o debugging do issue #9 no github
 	while (tam != 0) {
 		n_mesa += 1;
-		if (tam - rnd() < 0) {
+		if (tam - rnd < 0) {
 			inserir_mesas(mesa,n_mesa,tam);
 			tam = 0;
 		}
