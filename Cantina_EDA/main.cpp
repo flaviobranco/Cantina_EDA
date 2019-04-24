61fd35616e00983dee1b500d7ba82d1a68cd0596
Carlos Duarte Santos Gomes <cduartesg@hotmail.com>
2019-04-24 11:35:41 +0100

    Consertar a função while ao inserir mesas com "inserir_mesa"

--- Cantina_EDA/main.cpp
+++ Cantina_EDA/main.cpp
@@ -6,24 +6,24 @@ using namespace std;
 int main() {
 	//refeicao* refeicao; // criar refeição
 	//alterar_refeicao(refeicao);
-	int tam = 41;
+	int tam = rand()%30+20;
 	int n_mesa = 0;//número de mesas
 	int rnd=random_mesa();
-	int tam_ant;
+	int tam_ant=0;
 	mesa *mesa = new struct mesa[rnd];//criar a variável struct mesa com o nome "mesa", determinei um limite máximo para o debugging do issue #9 no github
 	while (tam != 0) {
-		n_mesa += 1;
-		if (tam - rnd < 0) {
-			n_mesa -= 1;
+		if(tam <= 1) {
 			inserir_mesas(mesa, n_mesa, 0);
-			tam = tam + tam_ant;
-		}
-		else {
-			inserir_mesas(mesa, n_mesa, rnd);
-			tam = tam - rnd;
+			n_mesa -= 1;
 			tam = tam_ant;
 		}
-		rnd = random_mesa();
+		else {
+			n_mesa += 1;
+			inserir_mesas(mesa, n_mesa, rnd);
+			tam_ant = tam;
+			tam = tam - rnd;
+			rnd = random_mesa();
+		}
 	}
 	mostrar_mesas(mesa, n_mesa);
 	return 0;
